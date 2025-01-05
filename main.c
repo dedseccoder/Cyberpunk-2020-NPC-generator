f94d9f4b0e88d7c5f2e9c17098505f019e6ad0c8
dedseccoder <andrey.flach2@gmail.com>
2025-01-05 03:23:37 +0300

    added stats generator

--- main.c
+++ main.c
@@ -7,16 +7,9 @@ struct character
 	char sex [100];
 	char name [100];
 	char role [100];
+	int boss;
    
-	int intelligence;
-	int reflex;
-	int tech;
-	int cool;
-	int sexuality;
-	int luck;
-	int move;
- 	int body;
-	int empathy;
+	int stats[9];
 	struct weapon; 
 	struct armor;
 };
@@ -42,34 +35,60 @@ struct cybernatic
 	int bonus;
 };
 
-void stats();
-void skills();
+void stats(int*);
+void skills(char*, int);
 void implants();
 void weapon();
 void armor();
 
 int main()
 {
+	struct character NPC;
 	int role, sex, boss;
-	srand(time(0));
-	int ref = rand() % (10 + 1 - 2) + 2;
-	printf("rand is: %i\n", ref);
-	printf("1.Rockerboy\n2.Solo\n3.Netrunner\n4.Techie\n5.Medtech\n6.Media\n7.Corporate\n8.Cop\n9.Fizer\n10.Nomad\n11.Random\n");   
+	char roles[10][40] = {"Rockerboy", "Solo", "Netrunner", "Techie", "Medtech", "Media", "Corporate", "Cop", "Fixer", "Nomad"};
+	for(int i = 0; i < 10; i++)
+	{
+		printf("%d.%s\n", i+1, roles[i]);
+	}
+	printf("11.Random\n");
 	printf("Choose your NPC role: "); 
 	scanf("%d", &role);
+	if(role == 11) 
+	{
+		srand(time(0));
+		role = rand() % (10 + 1 - 1) + 1;
+	}
 	printf("\n");
 	printf("1.Male\n2.Female\n3.Diffrent\n4.Random (male or female)\n");
 	printf("Set your NPC sex: ");
 	scanf("%d", &sex);
+	if(sex == 4) 
+	{
+		srand(time(0));
+		sex = rand() % (2 + 1 - 1) + 1;
+	}
 	printf("\n");
 	printf("Is your NPC advanced booster?\n");
 	printf("1.Yes\n2.No\n3.Random\n");
 	printf("Set: ");
 	scanf("%d", &boss);
+	if(boss == 3) 
+	{
+		srand(time(0));
+		boss = rand() % (2 + 1 - 1) + 1;
+	}
 	printf("\n");
 
-	role == 1 && sex == 2 ? printf("works fine\n") : printf("not working fine\n");
-
+	stats(NPC.stats);
+	
 	return 0;
 }
 
+void stats(int* statsArr)
+{
+	srand(time(0));
+	for(int i = 0; i < 9; i++)
+	{
+		statsArr[i] = rand() % (10 + 1 - 2) + 2;
+	}
+}
